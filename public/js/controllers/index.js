92ffc1754fd2e6cc23ddb1b679a82658754dee40
Lea Ament <lea@linnovate.net>
2013-11-17 19:06:19 +0200

    slide_2_categories

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,3 +1,19 @@
 angular.module('mean.system').controller('IndexController', ['$scope', 'Global', '$rootScope', function ($scope, Global, $rootScope) {
     $scope.global = Global;
+    $scoope.urllArr[
+    	{"sec.png"},{"eco.1.png"},{"fami.1.png"}
+    ];
+    alert("lala");
+    app.directive('backImg', function(){
+    return function(scope, element, attrs){
+        attrs.$observe('backImg', function(value) {
+            element.css({
+                'background-image': 'url(' + value +')',
+                'background-size' : 'cover'
+            });
+        });
+    };
+});
+
+
 }]);
