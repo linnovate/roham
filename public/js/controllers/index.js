3d1e35e67143231df4acdb47f3ef11ebe31ce95c
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2013-11-21 15:14:27 +0200

    more css fixes

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,7 +1,7 @@
 angular.module('mean.system').controller('IndexController', ['$scope', 'Global', 'Slides', '$rootScope',
     function($scope, Global, Slides, $rootScope) {
         $scope.global = Global;
-        $scope.slides = Slides;
+        $scope.slides = angular.copy(Slides);
 
         $scope.moveToTop = function(index){
             Slides = Slides.moveToTop(index);
