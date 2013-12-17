51db5aaacad9d40b9aca20d9d0134829b4ddb5f9
Lea Ament <lea@linnovate.net>
2013-12-17 15:15:20 +0200

    slide_13

--- public/js/controllers/slides.js
+++ public/js/controllers/slides.js
@@ -1,5 +1,5 @@
-angular.module('mean.slides').controller('SlidesController', ['$scope', '$routeParams', '$location', 'Global', 'Slides',
-    function($scope, $routeParams, $location, Global, Slides) {
+angular.module('mean.slides').controller('SlidesController', ['$scope', '$routeParams', '$location', 'Global', 'Slides','$http',
+    function($scope, $routeParams, $location, Global, Slides, $http) {
         $scope.global = Global;
 
         $scope.transitionClass = Global.transitionClass;
@@ -19,6 +19,38 @@ angular.module('mean.slides').controller('SlidesController', ['$scope', '$routeP
 
             Slides.saveSlideAnswers($scope.currentSlide,$scope.questions,Global.session_id);
             Slides.updateCurrentSlide(next,next_index);
-        };       
+        };
+
+        $scope.getInitEndPage = function() {
+          console.log('lalala');
+          $http.get('/cms/views/slide-13').success(function(slide) {
+            console.log(slide);
+            $scope.view = slide;
+            console.log($scope.view);
+
+          });
+        };
+
+
+/*                function getQuestions() {
+            $http.get('/cms/views').success(function(slides) {
+                var newSlides = {};
+                slides.forEach(function(slide){
+                    var questions = slide.fields,
+                        newQuestions = [];
+                        questions.forEach(function(question){
+                        newQuestions.push({
+                            title: question.name,
+                            body: question.label,
+                            val: 0
+                        });
+                    });
+
+                    newSlides[slide.slideId] = newQuestions;
+                });
+                _this._obj.questions = newSlides;
+            });
+        }*/
+
     }
 ]);
